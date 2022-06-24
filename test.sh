f8f6bea2b9833de514257293a61215f315701452
zyr17 <jzjqz17@gmail.com>
2022-06-24 21:19:56 +0800

    fix: error on deploy

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose.yml -f docker-compose-test.yml up --abort-on-container-exit --exit-code-from fastapi
+docker-compose -f docker-compose-test.yml up --abort-on-container-exit --exit-code-from fastapi_test
 exit $?
