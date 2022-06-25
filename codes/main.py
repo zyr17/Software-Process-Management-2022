f9837dd5efd7d55cec13eba65b29e87f12dd9f30
zyr17 <jzjqz17@gmail.com>
2022-06-25 13:24:05 +0800

    fix: CORS

--- codes/main.py
+++ codes/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 import apis.account
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 app.include_router(apis.account.router)
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get('/')
