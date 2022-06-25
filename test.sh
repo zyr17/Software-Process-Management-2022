e465ab6e4bdc466c1b60742ba1b1d877dda99b35
zyr17 <jzjqz17@gmail.com>
2022-06-25 13:38:16 +0800

    fix: test error

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+docker-compose -f docker-compose-test.yml up --abort-on-container-exit --exit-code-from fastapi
+RET=$?
 docker-compose -f docker-compose-test.yml down
-docker-compose -f docker-compose-test.yml up --abort-on-container-exit --exit-code-from fastapi_test
-exit $?
+exit $RET
