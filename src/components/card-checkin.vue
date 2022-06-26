dddf8cb707501a5466660d23aab6987401be8f8a
zyr17 <jzjqz17@gmail.com>
2022-06-26 20:17:22 +0800

    fix: card checkin

--- src/components/card-checkin.vue
+++ src/components/card-checkin.vue
@@ -6,36 +6,31 @@
 
     <div>
 
+      <div>
+        <el-select v-model="selectedBuilding" placeholder="选择楼栋">
+          <el-option 
+            v-for="i in buildingList" 
+            :key="i" 
+            :label="i" 
+            :value="i"
+          ></el-option>
+        </el-select>
+      </div>
+
+      <div>
+        <el-select v-model="selectedClassroom" placeholder="选择教室">
+          <el-option v-for="i in classRoomList" :key="i" :label="i" :value="i"></el-option>
+        </el-select>
+      </div>
+
       <form v-on:submit.prevent="checkin">
-        <div class="form-group">
-          <label name="building_number">楼栋</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="buildingNumber"
-            placeholder="手动填写，如：JB, GHX, Z"
-            id="building_number"
-            required
-          />
-        </div>
-        <div class="form-group">
-          <label name="classroom_number">教室</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="classRoomNumber"
-            placeholder="手动填写，如：JB101, GHX502, Z2201"
-            id="classroom_number"
-            required
-          />
-        </div>
         <div class="form-group">
           <label name="card_id">卡号</label>
           <input
             type="text"
             class="form-control"
-            v-model="cardId"
-            placeholder="刷卡自动填写(未实现)"
+            v-model="userId"
+            placeholder="刷卡自动填写(手动填写用户ID)"
             id="card_id"
             required
           />
@@ -57,35 +52,77 @@ import { backend_link } from "../const.vue";
 
 import store from '../store';
 
-let auth = store.state.auth;
-
-
 export default {
   data() {
     return {
+      originalStudyRooms: [],
+      selectedBuilding: '',
+      selectedClassroom: '',
       buildingNumber: '',
       classRoomNumber: '',
       notifications: [],
-      cardId: '',
+      userId: '',
     };
   },
   
-  created: function () {},
+  created: function () {
+    this.$http.get(backend_link + 'studyroom', {
+      headers: {
+        'Auth-Token': store.state.auth
+      }
+    }).then(
+      (response) => {
+        let data = response.data;
+        this.originalStudyRooms = data;
+      }
+    )
+  },
+
+  computed: {
+    selectedId () {
+      for (let i of this.originalStudyRooms)
+        if (i.buildingNumber == this.selectedBuilding
+            && i.classRoomNumber == this.selectedClassroom)
+          return i.id
+      return null
+    },
+    buildingList () {
+      let arr = [];
+      for (let i of this.originalStudyRooms)
+        arr.push(i.buildingNumber)
+      arr = Array.from(new Set(arr))
+      arr.sort()
+      return arr
+    },
+    classRoomList () {
+      let arr = [];
+      for (let i of this.originalStudyRooms)
+        if (i.buildingNumber == this.selectedBuilding)
+          arr.push(i.classRoomNumber)
+      arr = Array.from(new Set(arr))
+      arr.sort()
+      if (arr.indexOf(this.selectedClassroom) == -1)
+        this.selectedClassroom = ''
+      return arr
+    },
+  },
 
   methods: {
     checkin () {
       this.$http.post(backend_link + "card_checkin", {
-        buildingNumber: this.buildingNumber,
-        classRoomNumber: this.classRoomNumber,
-        cardId: this.cardId
+        roomId: this.selectedId,
+        userId: this.userId,
       }, {
         headers: {
-          "Content-Type": "application/json",
+          'Auth-Token': store.state.auth
         },
       })
       .then(
         (response) => {
-          let text = response.name + ' 座位号' + response.seatNumber + ' 时间' + response.startTime + '-' + response.endTime;
+          response = response.data
+          console.log(response)
+          let text = response.userName + ' '
+                     + '时间 ' + this.to_date(response.date) + ' ' + this.to_time(response.startTime) + '-' + this.to_time(response.endTime);
           this.notifications.push({
             type: "success",
             message: "签到成功\n" + text,
@@ -94,10 +131,17 @@ export default {
         (response) => {
           this.notifications.push({
             type: "danger",
-            message: "签到失败 " + JSON.stringify(response),
+            message: "签到失败 " + JSON.stringify(response.body.detail),
           });
         }
       );
+    },
+    to_date(i) {
+      let date = new Date(i * 1000 * 86400)
+      return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate()
+    },
+    to_time(i) {
+      return (i < 10 ? '0' : '') + (i) + ':00';
     },
   },
 
