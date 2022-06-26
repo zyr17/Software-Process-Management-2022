e3c46e402c00dda227e283f1300edac1c9f92cbf
zyr17 <jzjqz17@gmail.com>
2022-06-26 13:48:18 +0800

    fix: build image error

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+docker-compose -f docker-compose-test.yml build
 docker-compose -f docker-compose-test.yml up --abort-on-container-exit --exit-code-from fastapi
 RET=$?
 docker-compose -f docker-compose-test.yml down
