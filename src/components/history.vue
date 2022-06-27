ce6120f987b5b552a1cc1cd83efbc5634585ff3a
zyr17 <jzjqz17@gmail.com>
2022-06-27 10:42:35 +0800

    fix: get history bug

--- src/components/history.vue
+++ src/components/history.vue
@@ -11,7 +11,7 @@
           <!-- <td>座位号</td> -->
           <td>预定时间</td>
           <td>自习时间</td>
-          <td>签到时间</td>
+          <td>签到/取消时间</td>
           <td>状态</td>
           <td></td>
         </tr>
@@ -21,10 +21,12 @@
         <tr v-for="history in histories">
           <td>{{ history.buildingNumber }} {{ history.classRoomNumber }}</td>
           <!-- <td>{{ history.seatNumber }}</td> -->
-          <td>{{ history.bookTime }}</td>
-          <td>{{ to_time(history.startTime) }}-{{ to_time(history.endTime + 1) }}</td>
-          <td>{{ history.checkinTime }}</td>
-          <td>{{ statusName[history.status] }}</td>
+          <td>{{ new Date(history.bookTimeStamp) }}</td>
+          <td>{{ to_date(history.date) }} {{ to_time(history.startTime) }}-{{ to_time(history.endTime + 1) }}</td>
+          <td v-if="history.type == 'booked'"></td>
+          <td v-if="history.type == 'checkin'">{{ Date(history.checkinTimeStamp) }}</td>
+          <td v-if="history.type == 'cancel'">{{ Date(history.cancelTimeStamp) }}</td>
+          <td>{{ statusName[history.type] }}</td>
           <td>
             <button
               @click="reBook(history)"
@@ -61,63 +63,67 @@ export default {
   data() {
     return {
       statusName: {
-        'booking': '预定中',
-        'cancelled': '已取消',
-        'done': '已完成',
+        'booked': '预定中',
+        'cancel': '已取消',
+        'checkin': '已签到',
         'failed': '已爽约'
       },
       histories: [
-          {
-            id: 1,
-            buildingNumber: 'JB',
-            classRoomNumber: '101',
-            seatNumber: '25',
-            bookTime: '2022-06-01 14:00',
-            startTime: 14,
-            endTime: 14,
-            checkinTime: '-',
-            status: 'booking',
-          },
-          {
-            id: 2,
-            buildingNumber: 'JB',
-            classRoomNumber: '101',
-            seatNumber: '25',
-            bookTime: '2022-04-01 14:00',
-            startTime: 14,
-            endTime: 14,
-            checkinTime: '2022-04-01 13:57',
-            status: 'done',
-          },
-          {
-            id: 3,
-            buildingNumber: 'JA',
-            classRoomNumber: '202',
-            seatNumber: '5',
-            bookTime: '2022-03-01 14:00',
-            startTime: 14,
-            endTime: 14,
-            checkinTime: '-',
-            status: 'cancelled',
-          },
-          {
-            id: 4,
-            buildingNumber: 'JA',
-            classRoomNumber: '202',
-            seatNumber: '5',
-            bookTime: '2022-03-01 14:00',
-            startTime: 14,
-            endTime: 14,
-            checkinTime: '-',
-            status: 'failed',
-          }
+          // {
+          //   id: 1,
+          //   buildingNumber: 'JB',
+          //   classRoomNumber: '101',
+          //   seatNumber: '25',
+          //   bookTime: '2022-06-01 14:00',
+          //   startTime: 14,
+          //   endTime: 14,
+          //   checkinTime: '-',
+          //   status: 'booking',
+          // },
+          // {
+          //   id: 2,
+          //   buildingNumber: 'JB',
+          //   classRoomNumber: '101',
+          //   seatNumber: '25',
+          //   bookTime: '2022-04-01 14:00',
+          //   startTime: 14,
+          //   endTime: 14,
+          //   checkinTime: '2022-04-01 13:57',
+          //   status: 'done',
+          // },
+          // {
+          //   id: 3,
+          //   buildingNumber: 'JA',
+          //   classRoomNumber: '202',
+          //   seatNumber: '5',
+          //   bookTime: '2022-03-01 14:00',
+          //   startTime: 14,
+          //   endTime: 14,
+          //   checkinTime: '-',
+          //   status: 'cancelled',
+          // },
+          // {
+          //   id: 4,
+          //   buildingNumber: 'JA',
+          //   classRoomNumber: '202',
+          //   seatNumber: '5',
+          //   bookTime: '2022-03-01 14:00',
+          //   startTime: 14,
+          //   endTime: 14,
+          //   checkinTime: '-',
+          //   status: 'failed',
+          // }
       ],
       notifications: [],
     };
   },
   
   created: function () {
-    this.$http.get(backend_link + 'history', {}).then(
+    this.$http.get(backend_link + 'history/' + store.state.id, {
+      headers: {
+        'Auth-Token': store.state.auth
+      }
+    }).then(
       (response) => {
         this.histories = response.data;
       }
@@ -135,7 +141,7 @@ export default {
       this.$http
         .delete(backend_link + "history/" + history.id, {
           headers: {
-            "Content-Type": "application/json",
+            'Auth-Token': store.state.auth
           },
         })
         .then(
@@ -145,10 +151,14 @@ export default {
           (response) => {
             this.notifications.push({
               type: "danger",
-              message: "历史记录删除失败",
+              message: "历史记录删除失败 " + JSON.stringify(response.body.detail),
             });
           }
         );
+    },
+    to_date(i) {
+      let date = new Date(i * 1000 * 86400)
+      return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate()
     },
     to_time(i) {
       return (i < 10 ? '0' : '') + (i) + ':00';
