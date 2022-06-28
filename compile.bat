17f0376598dc61cdf232def38d2c5c7acb9555b9
zyr17 <jzjqz17@gmail.com>
2022-06-28 13:22:06 +0800

    init

---
+++ compile.bat
@@ -0,0 +1,4 @@
+xelatex.exe -synctex=1 -interaction=nonstopmode main.tex 
+bibtex main 
+xelatex.exe -synctex=1 -interaction=nonstopmode main.tex 
+xelatex.exe -synctex=1 -interaction=nonstopmode main.tex
